b8c41188507b8ad80a85c53ae32f58470cf581ed
zeFresk <8807862+zeFresk@users.noreply.github.com>
2025-08-14 19:51:42 +0200

    Add securities inside Circuit to ensure observables and gates match circuit size.

--- include/circuit.hpp
+++ include/circuit.hpp
@@ -163,6 +163,9 @@ class Circuit {
 	 * @see Observable::expectation_value()
 	 */
 	Observable<Coefficient_t> run(Observable<Coefficient_t> const& target_observable) {
+		if (target_observable[0].size() != nb_qubits()) {
+			throw std::invalid_argument("Number of qubits of the circuit doesn't match observable.");
+		}
 		auto obs = target_observable;
 		SimulationState state(nb_splitting_gates());
 		for (auto const& qop : std::ranges::reverse_view{ operations_ }) {
@@ -264,6 +267,7 @@ class Circuit {
 	 * @snippet tests/readme.cpp basic
 	 */
 	void add_operation_internal(QGate g, unsigned qubit) {
+		check_qubit(qubit);
 		if (in_array(g, pg_map) != std::cend(pg_map)) {
 			auto it = in_array(g, pg_map);
 			register_op(g, [=](O_t& obs) { obs.apply_pauli(it->second, qubit); });
@@ -289,6 +293,7 @@ class Circuit {
 	 */
 	template <typename Real, std::enable_if_t<std::is_floating_point_v<Real>, bool> = true>
 	void add_operation_internal(QGate g, unsigned qubit, Real c) {
+		check_qubit(qubit);
 		switch (g) {
 		case QGate::Rz:
 			register_op(g, [=](O_t& obs) { obs.apply_rz(qubit, c); });
@@ -318,12 +323,20 @@ class Circuit {
 	 */
 	template <typename Integer, std::enable_if_t<std::is_integral_v<Integer>, bool> = true>
 	void add_operation_internal(QGate g, unsigned control, Integer target) {
+		check_qubit(control);
+		check_qubit(target);
 		switch (g) {
 		case QGate::Cx:
 			register_op(g, [=](O_t& obs) { obs.apply_cx(control, target); });
 			break;
 		default:
 			throw std::invalid_argument("Unsupported gate type with those arguments");
+		}
+	}
+
+	void check_qubit(unsigned qubit) const {
+		if (qubit < 0 || qubit >= nb_qubits()) {
+			throw std::invalid_argument("Qubit index out of range for this circuit.");
 		}
 	}
 };
