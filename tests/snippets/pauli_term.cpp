891778bb47611d13f77f441865c87106a2345bf8
zeFresk <8807862+zeFresk@users.noreply.github.com>
2025-08-28 20:41:31 +0200

    pauliterm doc

--- tests/snippets/pauli_term.cpp
+++ tests/snippets/pauli_term.cpp
@@ -1,25 +1,23 @@
 #include "pauli_term.hpp"
-#include "pauli.hpp"
-#include <vector>
+#include <iostream>
 
-#include "gtest/gtest.h"
+void pauli_term_snippets() {
+    //! [pauli_term_from_string]
+    // From a string_view and a coefficient
+    auto pt_from_string = PauliTerm("IXYZ", 0.5f);
+    std::cout << "From string: " << pt_from_string << std::endl;
+    //! [pauli_term_from_string]
 
-void pauli_term_constructor_snippets() {
-	//! [pauli_term_from_string]
-	// From a string_view and a coefficient
-	auto pt_from_string = PauliTerm("IXYZ", 0.5f);
-	//! [pauli_term_from_string]
+    //! [pauli_term_from_list]
+    // From an initializer_list of Pauli objects
+    using enum Pauli_enum;
+    auto pt_from_list = PauliTerm({ I, X, Y, Z }, -1.0f);
+    std::cout << "From list: " << pt_from_list << std::endl;
+    //! [pauli_term_from_list]
 
-	//! [pauli_term_from_list]
-	// From an initializer_list of Pauli objects
-	using enum Pauli_enum;
-	auto pt_from_list = PauliTerm({ I, X, Y, Z }, -1.0f);
-	//! [pauli_term_from_list]
-
-	//! [pauli_term_from_pauli]
-	// From a single Pauli object
-	auto pt_from_pauli = PauliTerm(p_x, 1.0f);
-	//! [pauli_term_from_pauli]
+    //! [pauli_term_from_pauli]
+    // From a single Pauli object (for a 1-qubit term)
+    auto pt_from_pauli = PauliTerm(p_x, 1.0f);
+    std::cout << "From Pauli object: " << pt_from_pauli << std::endl;
+    //! [pauli_term_from_pauli]
 }
-
-TEST(documentation, PauliTerm) { pauli_term_constructor_snippets(); }
